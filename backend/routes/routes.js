d6520c086fc713baacdc92c7b52791b9fb2ce968
5Talib <talib.khan.cer20@iitbhu.ac.in>
2024-03-28 13:44:21 +0530

    Added vercel.json

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { showAvailableCars, bookCar, getAllCars, sentMail } = require("../controllers/controller");
 
+app.get("/", (req,res)=>{
+    res.send("You are on backend server");
+})
+
 router.route("/available-cars").post(showAvailableCars);
 router.route("/book-car").post(bookCar);
 router.route("/sent-mail").post(sentMail);
